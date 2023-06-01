c7ac852be7e3895af4f8688ad9739e4bca37d46a
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-06-01 16:08:49 +0900

    rename names of robot and joints

--- node/wheelcontroller.py
+++ node/wheelcontroller.py
@@ -3,8 +3,8 @@ import numpy as np
 import os
 
 import rospy
-from geometry_msgs.msg import Twist 
-from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint 
+from geometry_msgs.msg import Twist
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from sensor_msgs.msg import JointState
 
 import yaml
@@ -13,24 +13,23 @@ class WheelController:
     def __init__(self, wheelconfigfile):
         with open(wheelconfigfile) as f:
             self.wheelconfig = yaml.safe_load(f)
-        
-        robotname = self.wheelconfig["robot_name"]
+
         controllername = self.wheelconfig["controller_name"]
 
         self.present_wheel_angle = [0.0 for wheel in self.wheelconfig["wheels"]]
         self.send_length = 10
 
-        self.publisher = rospy.Publisher('/{}/{}/command'.format(robotname, controllername), JointTrajectory, queue_size=1)
-        rospy.Subscriber("/{}/cmd_vel".format(robotname), Twist , self.odom_callback)
-        rospy.Subscriber("/{}/joint_states".format(robotname), JointState, self.joint_callback)
+        self.publisher = rospy.Publisher('{}/command'.format(controllername), JointTrajectory, queue_size=1)
+        rospy.Subscriber("cmd_vel", Twist , self.com_vel_callback)
+        rospy.Subscriber("joint_states", JointState, self.joint_callback)
 
     def joint_callback(self, data):
         for i, wheel in enumerate(self.wheelconfig["wheels"]):
             for j in range(len(data.name)):
                 if data.name[j] == wheel["name"]:
                     self.present_wheel_angle[i] = data.position[j]
-                    
-    def odom_callback(self, data):
+
+    def com_vel_callback(self, data):
         w_list = []
         for wheel in self.wheelconfig["wheels"]:
             v = data.linear.x * np.dot([0,1], wheel["rot_axis"][0:2]) + data.linear.y * np.dot([-1,0], wheel["rot_axis"][0:2])
@@ -48,7 +47,7 @@ class WheelController:
                 point.positions.append(w*j + self.present_wheel_angle[i])
             point.time_from_start=rospy.Time.from_sec(j)
             pub_msg.points.append(point)
-        self.publisher.publish(pub_msg)     
+        self.publisher.publish(pub_msg)
 
 if __name__=='__main__':
     rospy.init_node("wheel_controller")
