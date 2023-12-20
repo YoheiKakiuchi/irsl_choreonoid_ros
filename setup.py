7bf94dce9ecaa7c0bf41b5e092a6ac2bd927c86b
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-12-20 18:58:40 +0900

    move directory for installing with ROS2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['irsl_choreonoid_ros'],
-    package_dir={'': 'src'},
+    package_dir={'irsl_choreonoid_ros': 'irsl_choreonoid_ros'},
 )
 
 setup(**setup_args)
